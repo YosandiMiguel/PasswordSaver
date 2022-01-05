d984b925c50b29424d4eeb36dd1ea27e172fc262
Yosandi <elyosandireyes@gmail.com>
2022-01-05 17:42:45 -0400

    Version 0

---
+++ Model/DAO/ConeccionDAO.cs
@@ -0,0 +1,14 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordSaver2._0.Model.DAO
+{
+    class ConeccionDAO
+    {
+        protected MySqlConnection coneccion = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password= ;database=db_Prueba_Password;");
+    }
+}
---
+++ Model/DTO/Login/LoginDAO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordSaver2._0.Model.DTO.Login
+{
+    class LoginDAO
+    {
+        //Propiedades 
+        int _id;
+        string _nombre;
+        string _apellido;
+        string _correo;
+        string _contraseña;
+        DateTime _fecha_Creacion;
+
+     //Gets y Sets
+        public int ID
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Nombre
+        {
+            get => _nombre;
+            set => _nombre = value;
+        }
+
+        public string Apellido
+        {
+            get => _apellido;
+            set => _apellido = value;
+        }
+
+        public string Correo
+        {
+
+            get => _correo;
+            set => _correo = value;
+        }
+
+        public string Contraseña
+        {
+            get => _contraseña;
+            set => _contraseña = value;
+        }
+
+        public DateTime Fecha_Creacion
+        {
+            get => _fecha_Creacion;
+            set => _fecha_Creacion = value;
+        }
+    }
+}
