ba34fb7116c18b84dcd9a1fe6a8f10c71628821a
Yosandi <elyosandireyes@gmail.com>
2022-05-06 14:13:17 -0400

    Funcionalidades basicas y diseño del login completados

---
+++ View/Form1.designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAcceder = new System.Windows.Forms.Button();
+            this.linkPass = new System.Windows.Forms.LinkLabel();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(214, 286);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(0, 66);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(214, 165);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtUser
+            // 
+            this.txtUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtUser.Font = new System.Drawing.Font("Constantia", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txtUser.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUser.Location = new System.Drawing.Point(279, 85);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(321, 25);
+            this.txtUser.TabIndex = 1;
+            this.txtUser.Text = "USUARIO";
+
+            // 
+            // txtPass
+            // 
+            this.txtPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPass.Font = new System.Drawing.Font("Constantia", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txtPass.ForeColor = System.Drawing.Color.DimGray;
+            this.txtPass.Location = new System.Drawing.Point(279, 132);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(321, 25);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.Text = "CONTRASEÑA";
+
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Constantia", 25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(428, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 41);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "LOGIN";
+            // 
+            // btnAcceder
+            // 
+            this.btnAcceder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnAcceder.FlatAppearance.BorderSize = 0;
+            this.btnAcceder.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnAcceder.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAcceder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAcceder.ForeColor = System.Drawing.Color.LightGray;
+            this.btnAcceder.Location = new System.Drawing.Point(279, 196);
+            this.btnAcceder.Name = "btnAcceder";
+            this.btnAcceder.Size = new System.Drawing.Size(321, 35);
+            this.btnAcceder.TabIndex = 3;
+            this.btnAcceder.Text = "ACCEDER";
+            this.btnAcceder.UseVisualStyleBackColor = false;
+            // 
+            // linkPass
+            // 
+            this.linkPass.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.linkPass.AutoSize = true;
+            this.linkPass.Font = new System.Drawing.Font("Constantia", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.linkPass.LinkColor = System.Drawing.Color.DimGray;
+            this.linkPass.Location = new System.Drawing.Point(366, 250);
+            this.linkPass.Name = "linkPass";
+            this.linkPass.Size = new System.Drawing.Size(166, 14);
+            this.linkPass.TabIndex = 0;
+            this.linkPass.TabStop = true;
+            this.linkPass.Text = "¿Has olvidado tu contaseña?";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.btnCerrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.Location = new System.Drawing.Point(655, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(13, 13);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 6;
+            this.btnCerrar.TabStop = false;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMinimizar.Image = ((System.Drawing.Image)(resources.GetObject("btnMinimizar.Image")));
+            this.btnMinimizar.Location = new System.Drawing.Point(645, 0);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(13, 13);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimizar.TabIndex = 7;
+            this.btnMinimizar.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(669, 286);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.linkPass);
+            this.Controls.Add(this.btnAcceder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkPass;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        public System.Windows.Forms.TextBox txtUser;
+        public System.Windows.Forms.TextBox txtPass;
+        public System.Windows.Forms.Button btnAcceder;
+        public System.Windows.Forms.PictureBox btnCerrar;
+        public System.Windows.Forms.PictureBox btnMinimizar;
+    }
+}
+
--- controller/LoginController.cs
+++ controller/LoginController.cs
@@ -3,27 +3,155 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PasswordSaver2._0.Model.DAO;
-using PasswordSaver2._0.View;
 using System.Threading.Tasks;
+using Login;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace PasswordSaver2._0.Controller
 {
     class LoginController
     {
-        Login refence_view;
-
-        public LoginController(Login view)
+        Form1 viewController;
+        //Constructor
+        public LoginController(Form1 view)
         {
-            refence_view = view;
-            refence_view.Load += new EventHandler(Show);
-;        }
+            viewController = view;
 
-        private void Show(Object sender,EventArgs e)
-        {
-            UserDAO userInformation = new UserDAO();
+            //Form Events
+            #region Eventos de la forma
 
-            refence_view.DGVtest.DataSource = userInformation.Login("el_yosandi_reyes", "eab47a662ac21299cf96aba2ab1b1485a09b5ae2ef6d6e3a71736cb4704dfaba");
+            viewController.MouseDown += new MouseEventHandler(Move_Form);
+            viewController.btnMinimizar.Click += new EventHandler(Minimized);
+            viewController.btnCerrar.Click += new EventHandler(Close);
+            #endregion
+
+            //Btn close and minimized
+            #region botones cerrar y minimizar
+            viewController.btnCerrar.MouseEnter += new EventHandler(ChangeColor_BtnClose_MouseInto);
+            viewController.btnCerrar.MouseLeave += new EventHandler(ChangeColor_BtnClose_MouseOut);
+            viewController.btnMinimizar.MouseEnter += new EventHandler(ChangeColor_BtnMinimized_MouseInto);
+            viewController.btnMinimizar.MouseLeave += new EventHandler(ChangeColor_BtnMinimized_MouseOut);
+            #endregion
+
+            //Text Box
+            #region Cuadros de texto
+            viewController.txtUser.Enter += new EventHandler(Clean_TextBox_User);
+            viewController.txtUser.Leave += new EventHandler(Water_Mark_User);
+            viewController.txtPass.Enter += new EventHandler(Clean_Textbox_Password);
+            viewController.txtPass.Leave += new EventHandler(Water_Mark_Password);
+            #endregion
         }
 
+        //Move window
+        #region mover ventana 
+
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+
+        private void Move_Form(object sender, MouseEventArgs e)
+        {
+
+
+            ReleaseCapture();
+            SendMessage(viewController.Handle, 0x112, 0xf012, 0);
+
+        }
+
+        #endregion
+
+
+        //Close form and minimized
+        #region Cerrar y minimizar forma
+        private void Minimized(object sender, EventArgs e)
+        {
+            viewController.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Close(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+
+        //Change color with the mouse
+        #region cambio de color con el puntero
+
+        private void ChangeColor_BtnClose_MouseInto(object sender, EventArgs e)
+        {
+            viewController.btnCerrar.BackColor = Color.FromArgb(30, 30, 30);
+        }
+
+        private void ChangeColor_BtnClose_MouseOut(object sender, EventArgs e)
+        {
+            viewController.btnCerrar.BackColor = Color.FromArgb(15, 15, 15);
+        }
+
+        private void ChangeColor_BtnMinimized_MouseInto(object sender, EventArgs e)
+        {
+            viewController.btnMinimizar.BackColor = Color.FromArgb(30, 30, 30);
+        }
+
+        private void ChangeColor_BtnMinimized_MouseOut(object sender, EventArgs e)
+        {
+            viewController.btnMinimizar.BackColor = Color.FromArgb(15, 15, 15);
+        }
+        #endregion
+
+        //Text boxes
+        #region Cuadros de texto
+
+        private void Clean_TextBox_User(object sender, EventArgs e)
+        {
+            if (viewController.txtUser.Text == "USUARIO")
+            {
+
+                viewController.txtUser.Text = string.Empty;
+                viewController.txtUser.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void Water_Mark_User(object sender, EventArgs e)
+        {
+            if (viewController.txtUser.Text == string.Empty)
+            {
+                viewController.txtUser.Text = "USUARIO";
+                viewController.txtUser.ForeColor = Color.DimGray;
+            }
+        }
+
+
+        private void Clean_Textbox_Password(object sender, EventArgs e)
+        {
+            if (viewController.txtPass.Text == "CONTRASEÑA")
+            {
+                viewController.txtPass.Text = string.Empty;
+                viewController.txtPass.UseSystemPasswordChar = true;
+                viewController.txtPass.ForeColor = Color.LightGray;
+
+            }
+        }
+
+        private void Water_Mark_Password(object sender, EventArgs e)
+        {
+            if (viewController.txtPass.Text == string.Empty)
+            {
+                viewController.txtPass.UseSystemPasswordChar = false;
+                viewController.txtPass.Text = "CONTRASEÑA";
+                viewController.txtPass.ForeColor = Color.DimGray;
+            }
+        }
+        #endregion
     }
+
 }
+
+
