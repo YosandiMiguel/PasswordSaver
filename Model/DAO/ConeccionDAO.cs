cbbcdb12b86cefa90727e3e3b216f4f3a2628ee2
Yosandi <elyosandireyes@gmail.com>
2022-05-08 21:45:00 -0400

    String de coneccion en un AppConfig

--- Model/DAO/ConeccionDAO.cs
+++ Model/DAO/ConeccionDAO.cs
@@ -5,12 +5,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 using System.Threading.Tasks;
 
 namespace PasswordSaver2._0.Model.DAO
 {
     class ConeccionDAO
     {
-        protected MySqlConnection coneccion = new MySqlConnection("datasource=127.0.0.1;port=3306;username=root;password=;database=test;");
+        protected MySqlConnection coneccion = new MySqlConnection(ConfigurationManager.ConnectionStrings["SqlConection"].ConnectionString); 
     }
 }
